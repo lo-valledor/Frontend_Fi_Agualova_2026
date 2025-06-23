f6f08d5a9541e7c51dd39f82149727bd66200b4f
Gerard Bourguett <gbourguett@lovalledor.cl>
2025-06-23 11:59:33 -0400

    Arreglo menor

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,8 +1,6 @@
 // eslint-disable no-empty-pattern
 import { AutoRedirect } from '~/components/auth/auto-redirect';
 import type { Route } from './+types/home';
-import { useLayoutEffect } from 'react';
-import { useNavigate } from 'react-router';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,16 +8,6 @@ export function meta({}: Route.MetaArgs) {
     { name: 'description', content: 'Enerlova | Inicio' },
   ];
 }
-
-useLayoutEffect(() => {
-  const navigate = useNavigate();
-  const isAuth = sessionStorage.getItem('token');
-  if (isAuth) {
-    navigate('/dashboard');
-  } else {
-    navigate('/auth/login');
-  }
-});
 
 export default function Home() {
   return <AutoRedirect loadingMessage="Redirigiendo..." />;
