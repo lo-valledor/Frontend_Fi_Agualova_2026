4925c751da369381a592fccc657b58a7e3504e31
Gerard Bourguett <gbourguett@lovalledor.cl>
2025-06-24 11:52:09 -0400

    refactor: unificación de comillas y limpieza de código en columnas de corte-reposición
    - Se unificaron las comillas a comillas simples en el archivo columns.tsx para mantener la consistencia.
    - Se realizaron ajustes menores en la estructura del código para mejorar la legibilidad.

--- app/components/operaciones/corte-reposicion/columns.tsx
+++ app/components/operaciones/corte-reposicion/columns.tsx
@@ -1,72 +1,72 @@
-import type { ColumnDef } from "@tanstack/react-table";
-import { DataTableColumnHeader } from "~/components/data-table/data-table-column-header";
-import type { ConsultarMantenedorRevisionCorte } from "~/types/operaciones";
+import type { ColumnDef } from '@tanstack/react-table';
+import { DataTableColumnHeader } from '~/components/data-table/data-table-column-header';
+import type { ConsultarMantenedorRevisionCorte } from '~/types/operaciones';
 
 export const columns: ColumnDef<ConsultarMantenedorRevisionCorte>[] = [
   {
-    accessorKey: "ctId",
+    accessorKey: 'ctId',
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="ID" />
     ),
   },
   {
-    accessorKey: "seCodigo",
+    accessorKey: 'seCodigo',
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Código" />
     ),
   },
   {
-    accessorKey: "meNSerie",
+    accessorKey: 'meNSerie',
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="N° Serie" />
     ),
   },
   {
-    accessorKey: "clRut",
+    accessorKey: 'clRut',
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="RUT" />
     ),
   },
   {
-    accessorKey: "clRazonSocialCompleto",
+    accessorKey: 'clRazonSocialCompleto',
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Razón Social" />
     ),
   },
   {
-    accessorKey: "niDescripcion",
+    accessorKey: 'niDescripcion',
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Descripción" />
     ),
   },
   {
-    accessorKey: "secDescripcion",
+    accessorKey: 'secDescripcion',
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Sección" />
     ),
   },
   {
-    accessorKey: "reEstado",
+    accessorKey: 'reEstado',
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Estado" />
     ),
   },
   {
-    accessorKey: "reCantDocumentos",
+    accessorKey: 'reCantDocumentos',
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Cantidad de Documentos" />
     ),
   },
   {
-    accessorKey: "reDeudaTotal",
+    accessorKey: 'reDeudaTotal',
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Deuda Total" />
     ),
   },
   {
-    header: "Acciones",
-    cell: ({ row }) => {
-      // const periodo = row.original;  
+    header: 'Acciones',
+    cell: () => {
+      // const periodo = row.original;
       return (
         <div className="flex gap-2">
           {/* {periodo.epf_descripcion === "Abierto" ? (
